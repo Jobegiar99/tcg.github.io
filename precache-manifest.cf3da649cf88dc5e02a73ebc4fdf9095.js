ed8a9f01360f16008a32a0237fbe669650bf1686
José Bernardo Girón Arana <bernardo.giron99@gmail.com>
2020-08-08 23:15:02 -0600

    Updates

---
+++ precache-manifest.cf3da649cf88dc5e02a73ebc4fdf9095.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27469943294142bf119ee78b27a81ece",
+    "url": "/tcg.github.io/index.html"
+  },
+  {
+    "revision": "8f54a6f1b9166dd2f1e5",
+    "url": "/tcg.github.io/static/css/main.eaac28f6.chunk.css"
+  },
+  {
+    "revision": "14c092ca73c89c0fda7d",
+    "url": "/tcg.github.io/static/js/2.2413c378.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/tcg.github.io/static/js/2.2413c378.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f54a6f1b9166dd2f1e5",
+    "url": "/tcg.github.io/static/js/main.ed74a8cc.chunk.js"
+  },
+  {
+    "revision": "eab05b06fd3af85d73e9",
+    "url": "/tcg.github.io/static/js/runtime-main.0ab394ce.js"
+  }
+]);
